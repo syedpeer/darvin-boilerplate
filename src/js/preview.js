2534af39b8823995935831fdc0dd491022018aa7
tobias.frei <mail@tobiasfrei.ch>
2019-02-05 23:23:54 +0100

    add dependencies to tmpl context, add jsplumb, add connect button

--- src/js/preview.js
+++ src/js/preview.js
@@ -2,6 +2,8 @@ import '../styles/preview/main.scss';
 
 import headerBar from '../js/preview/header-bar.preview';
 import elementActivity from '../js/preview/element-activity.preview';
+import dependencies from '../js/preview/dependencies.preview';
 
 headerBar.init();
 elementActivity.init();
+dependencies.init();
